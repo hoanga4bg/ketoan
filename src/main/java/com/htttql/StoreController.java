3c2943b42aa692c4c1a40749a9e15b0f0bc4a591
hoanga4bg <hoanga4bg@gmail.com>
2021-05-06 23:48:59 +0700

    ok8

--- src/main/java/com/htttql/StoreController.java
+++ src/main/java/com/htttql/StoreController.java
@@ -1,16 +1,35 @@
 package com.htttql;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.htttql.entity.Store;
+import com.htttql.repository.ProductRepository;
+import com.htttql.repository.StoreRepository;
+
 @Controller
 
 @RequestMapping("/store")
 public class StoreController {
+	
+	@Autowired
+	private ProductRepository productRepo;
+	
+	
+	@Autowired
+	private StoreRepository storeRepo;
+	
 	@GetMapping
 	private String storeHome(Model model) {
+		List<Store> listStore=new ArrayList<Store>();
+		listStore=storeRepo.findAll();
+		model.addAttribute("listStore", listStore);
 		return "store/storehome";
 	}
 }
