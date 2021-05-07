ed1d3807e775e840928b3eb4164b7f782475fc0c
hai-hash <65753082+hai-hash@users.noreply.github.com>
2021-05-07 16:41:42 +0700

    update search bill by name

--- src/main/java/com/htttql/repository/AccountantRepository.java
+++ src/main/java/com/htttql/repository/AccountantRepository.java
@@ -1,5 +1,7 @@
 package com.htttql.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.htttql.entity.Accountant;
@@ -7,5 +9,6 @@ import com.htttql.entity.Accountant;
 public interface AccountantRepository extends JpaRepository<Accountant, Integer>{
 
 	Accountant findOneById(Integer id);
+	List<Accountant> findByName(String name);
 
 }
