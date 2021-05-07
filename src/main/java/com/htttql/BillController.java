fc5ae0ca40ed7946bf4f73b2bf820f83f9691fb3
hai-hash <65753082+hai-hash@users.noreply.github.com>
2021-05-07 19:49:34 +0700

    update search by date

--- src/main/java/com/htttql/BillController.java
+++ src/main/java/com/htttql/BillController.java
@@ -1,6 +1,10 @@
 package com.htttql;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.hibernate.annotations.Parameter;
@@ -62,5 +66,42 @@ public class BillController {
 		model.addAttribute("totalprice",totalprice);
 		
 		return "Bill/showbill";
+	}
+	
+	@RequestMapping(value = "/search/bill/by/date", method = RequestMethod.GET)
+	public String searchBillByDate(@RequestParam("datestart") String datestart, @RequestParam("dateend") String dateend,Model model) {
+		SimpleDateFormat sim = new SimpleDateFormat("yyyy-MM-dd");
+		Date startdate;
+		Date enddate;
+		try {
+			startdate = sim.parse(datestart);
+			enddate = sim.parse(dateend);
+			List<Bill> bills = new ArrayList<Bill>();
+			bills = billRepository.findAll();
+			List<Bill> listbills = new ArrayList<Bill>();
+			for (Bill bill : bills) {
+				if(((bill.getCreateDate().compareTo(startdate) == 1) && (bill.getCreateDate().compareTo(enddate) == -1)) || (bill.getCreateDate().compareTo(startdate) == 0) || (bill.getCreateDate().compareTo(enddate) == 0)) {
+					listbills.add(bill);
+				}
+			}
+			
+			double totalprice = 0;
+			
+			for (Bill bill : listbills) {
+				totalprice += bill.getTotalPrice();
+				
+			}
+			model.addAttribute("bills", listbills);
+			model.addAttribute("totalprice",totalprice);
+			
+			System.out.print(totalprice);
+			return "Bill/showbill";
+			
+		} catch (ParseException e) {
+			e.getStackTrace();
+			return "Bill/test";
+		}
+		
+		
 	}
 }
