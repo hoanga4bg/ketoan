4e82a2f11d2c1abad69c2eaa222516c1109b0ae3
hoanga4bg <hoanga4bg@gmail.com>
2021-05-07 21:25:17 +0700

    Update Orders.java

--- src/main/java/com/htttql/entity/Orders.java
+++ src/main/java/com/htttql/entity/Orders.java
@@ -18,6 +18,7 @@ public class Orders {
 	private Integer id;
 	private String customer;
 	private int amount;
+	private boolean status;
 	@ManyToOne
 	@JoinColumn(name = "product_id")
 	private Product product;
