044133fdd040b53642e5cb94094b2755c2690757
hai-hash <65753082+hai-hash@users.noreply.github.com>
2021-05-14 12:16:07 +0700

    update thêm nhân viên

--- src/main/java/com/htttql/AdminSalaryController.java
+++ src/main/java/com/htttql/AdminSalaryController.java
@@ -1,10 +1,14 @@
 package com.htttql;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.htttql.entity.Salary;
 import com.htttql.repository.SalaryRepository;
@@ -29,7 +33,30 @@ public class AdminSalaryController {
 		salary.setStatus(false);
 		System.out.print(salary.getNameStaff());
 		salaryRepository.save(salary);
-		return "redirect:/admin/create/salary";
+		return "redirect:/admin/staff";
+	}
+	@RequestMapping(value = "/admin/staff",method = RequestMethod.GET)
+	public String findAll(Model model) {
+		List<Salary> salarys = new ArrayList<Salary>();
+		salarys = salaryRepository.findAll();
+		model.addAttribute("salarys", salarys);
+		return "admin/salary/display";
+	}
+	
+	@RequestMapping(value = "/admin/staff/off",method = RequestMethod.GET)
+	public String setOffStaff(@RequestParam("id") String id) {
+		Salary salary = salaryRepository.findOneById(Integer.parseInt(id));
+		salary.setStatus(true);
+		salaryRepository.save(salary);
+		return "redirect:/admin/staff";
+	}
+	
+	@RequestMapping(value = "/admin/staff/on",method = RequestMethod.GET)
+	public String setOnStaff(@RequestParam("id") String id) {
+		Salary salary = salaryRepository.findOneById(Integer.parseInt(id));
+		salary.setStatus(false);
+		salaryRepository.save(salary);
+		return "redirect:/admin/staff";
 	}
 
 }
