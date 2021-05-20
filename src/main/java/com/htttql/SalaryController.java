01101927e95694bd372b4c7495741f834199aff6
hoanga4bg <hoanga4bg@gmail.com>
2021-05-20 12:54:12 +0700

    1

--- src/main/java/com/htttql/SalaryController.java
+++ src/main/java/com/htttql/SalaryController.java
@@ -2,6 +2,9 @@ package com.htttql;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
@@ -82,9 +85,62 @@ public class SalaryController {
 		List<HistorySalary> list=new ArrayList<HistorySalary>();
 		list=histRepo.findAll();
 		Collections.reverse(list);
+		Double total=0.0;
+		Double totalTax=0.0;
+		for(HistorySalary l:list) {
+			total+=l.getMoney();
+			if(l.getMoney()<=9000000) {
+				totalTax+=0;
+			}
+			else {
+				totalTax+=((l.getMoney()-9000000)*0.15);
+			}
+		}
+		model.addAttribute("total", total);
+		model.addAttribute("totalTax", totalTax);
 		model.addAttribute("list", list);
 		return "salary/paySalary";
 	}
+	
+	@GetMapping("/hispay/search")
+	public String hispaySearch(Model model,
+								@RequestParam("startdate") String startDate,
+								@RequestParam("enddate") String endDate) {
+		if(startDate.equals("")||endDate.equals("")) {
+			return "redirect:/salary/hispay";
+		}
+		else {
+			SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
+			DateTimeFormatter dtf= DateTimeFormatter.ofPattern("yyyy-MM-dd");
+			try {
+
+				LocalDate ed = LocalDate.parse(endDate, dtf).plusDays(1);
+				Date sDate=sdf.parse(startDate);
+				Date eDate=Date.from(ed.atStartOfDay(ZoneId.systemDefault()).toInstant());
+				List<HistorySalary> list=new ArrayList<HistorySalary>();
+				list=histRepo.findByReceiveDateBetween(sDate,eDate);
+				Double total=0.0;
+				Double totalTax=0.0;
+				for(HistorySalary l:list) {
+					total+=l.getMoney();
+					if(l.getMoney()<=9000000) {
+						totalTax+=0;
+					}
+					else {
+						totalTax+=((l.getMoney()-9000000)*0.15);
+					}
+				}
+				model.addAttribute("total", total);
+				model.addAttribute("totalTax", totalTax);
+				model.addAttribute("list", list);
+			}	
+			catch(Exception e){
+				
+			}
+		}
+		return "salary/paySalary";
+	}
+	
 	
 	@GetMapping("/detail")
 	public String detail(Model model,@RequestParam("id") String id) {
@@ -93,7 +149,7 @@ public class SalaryController {
 		list=histRepo.findBySalary(s);
 		Collections.reverse(list);
 		model.addAttribute("list", list);
-		return "salary/paySalary";
+		return "salary/detail";
 	}
 	@GetMapping("/search")
 	public String search(Model model, @RequestParam("position") String position) {
